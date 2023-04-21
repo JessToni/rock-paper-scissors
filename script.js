7dde9e6733fa82d9fe632eb489921a9e03c8db70
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-04-21 11:47:34 +0800

    Establish functions that get a computer choice and a function to get a random number

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hey");
+let num = 0;
+getRandomNumber();
+
+function getComputerChoice() {
+    getRandomNumber();
+}
+
+function getRandomNumber(min = 1, max = 3) {
+    let rand = Math.floor(Math.random() * max);
+}
