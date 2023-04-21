64097f2cd195b786309d4c6f95d93a06b8adab2c
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-04-21 11:54:50 +0800

    Add logical if, else if, and else operators to getComputerChoice function

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 let num = 0;
-getRandomNumber();
+getComputerChoice();
 
 function getComputerChoice() {
     getRandomNumber();
+    if (num === 0) {
+        return "Rock"
+    } else if (num === 1) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
 }
 
 function getRandomNumber(min = 1, max = 3) {
     let rand = Math.floor(Math.random() * max);
+    num = rand;
 }
