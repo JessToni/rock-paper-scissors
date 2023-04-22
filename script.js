d8f7282ec6e15733ae81c1129c3c58b7b1743d36
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-04-22 15:06:45 +0800

    Test if value from getComputerChoice is stored

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 let num = 0;
 const playerChoice = "rock";
-getComputerChoice();
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
 
 function getComputerChoice() {
     getRandomNumber();
     if (num === 0) {
-        return console.log("Rock");
+        return "Rock"
     } else if (num === 1) {
-        return console.log("Paper");
+        return "Paper"
     } else {
-        return console.log("Scissors");
+        return "Scissors"
     }
 }
 
 function getRandomNumber(min = 1, max = 3) {
     let rand = Math.floor(Math.random() * max);
     num = rand;
+}
+
+function playRound(playerSelection, computerSelection) {
+
 }
