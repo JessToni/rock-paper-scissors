812a3ab188b8f74872706d5c73bd2bab1f2b2981
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-04-23 11:37:15 +0800

    Test content of computerChoice inside of for loop

--- script.js
+++ script.js
@@ -43,6 +43,8 @@ function playRound(playerSelection,computerSelection) {
 
 function game() {
     for (let i = 0; i < 5; i++) {
+        computerChoice = getComputerChoice();
         console.log(playRound(playerChoice,computerChoice));
+        console.log(computerChoice);
     }
 }
