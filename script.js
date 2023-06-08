6aec19085cae58f1541681fc381b90e0e34521eb
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-06-08 11:03:17 +0800

    Replace switch to if else. Add scoring to buttons

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ const rockButton = document.querySelector('.rock');
 const paperButton = document.querySelector('.paper');
 const scissorsButton = document.querySelector('.scissors');
 const showResults = document.querySelector('.results');
-const playerScore = document.querySelector('.player-score');
-const computerScore = document.querySelector('.computer-score')
+const showPlayerScore = document.querySelector('.player-score');
+const showComputerScore = document.querySelector('.computer-score')
 const gameWinner = document.querySelector('.game-winner')
+
+let playerScore = 0;
+let computerScore = 0;
+
 
 function getComputerChoice() {
     const arrOfChoices = ['rock', 'paper', 'scissors']
@@ -21,38 +25,49 @@ function playRound(playerSelection,computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
 
-    switch (true) {
-        case (playerSelection === computerSelection):
-            return "It's a draw";
-        case (playerSelection === "scissors" && computerSelection === "paper"):
-            return "Player wins!";
-        case (playerSelection === "scissors" && computerSelection === "rock"):
-            return "Computer wins!";
-        case (playerSelection === "paper" && computerSelection === "rock"):
-            return "Player wins!";
-        case (playerSelection === "paper" && computerSelection === "scissors"):
-            return "Computer wins!";
-        case (playerSelection === "rock" && computerSelection === "scissors"):
-            return "Player wins!";
-        case (playerSelection === "rock" && computerSelection === "paper"):
-            return "Computer wins!";
+    if (playerSelection === computerSelection) {
+        return "Nobody wins. It's a draw!"
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++;
+        return "Scissors cuts paper. Player wins!"
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore++;
+        return "Rock smashes scissors. Computer wins!"
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore++;
+        return "Paper trumps rock. Player wins!"
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore++;
+        return "Scissors cuts paper. Computer wins!"
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore++;
+        return "Paper trumps rock. Computer wins!"
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore++;
+        return "Rock smashes scissors. Player wins!"
     }
 }
 
 rockButton.addEventListener('click', () => {
     const computerSelection = getComputerChoice();
     const playerSelection = 'rock';
+    showPlayerScore.textContent = "Player Score: " + playerScore;
+    showComputerScore.textContent = "Computer Score: " + computerScore;
     showResults.textContent = playRound(playerSelection, computerSelection);
 })
 
 paperButton.addEventListener('click', () => {
     const computerSelection = getComputerChoice();
     const playerSelection = 'paper';
+    showPlayerScore.textContent = "Player Score: " + playerScore;
+    showComputerScore.textContent = "Computer Score: " + computerScore;
     showResults.textContent = playRound(playerSelection, computerSelection);
 })
 
 scissorsButton.addEventListener('click', () => {
     const computerSelection = getComputerChoice();
     const playerSelection = 'scissors';
+    showPlayerScore.textContent = "Player Score: " + playerScore;
+    showComputerScore.textContent = "Computer Score: " + computerScore;
     showResults.textContent = playRound(playerSelection, computerSelection);
 })
